db0feefc7e8e18ec509d55f6a3fdbdc42c7181ed
Raphael Torquato <89878688+raphaeltorquat0@users.noreply.github.com>
2022-06-08 23:01:52 -0300

    Requesting Permission to Access Photo Library
    Requesting Permission to Access Photo Library

--- rxswift-camera-filter/Controller/PhotosCollectionViewController.swift
+++ rxswift-camera-filter/Controller/PhotosCollectionViewController.swift
@@ -1,6 +1,19 @@
 import Foundation
 import UIKit
+import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populatePhotos()
+    }
+    
+    private func populatePhotos() {
+        PHPhotoLibrary.requestAuthorization { status in
+            if status == .authorized {
+                /*Access the hphotos from photo library*/
+            }
+        }
+    }
 }
