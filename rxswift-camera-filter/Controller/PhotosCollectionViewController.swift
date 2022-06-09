e25754b12a4a584d3cda2d43f8ca19800740ca48
Raphael Torquato <89878688+raphaeltorquat0@users.noreply.github.com>
2022-06-08 23:07:51 -0300

    Fetching All Photo Assets
    Fetching All Photo Assets

--- rxswift-camera-filter/Controller/PhotosCollectionViewController.swift
+++ rxswift-camera-filter/Controller/PhotosCollectionViewController.swift
@@ -4,15 +4,22 @@ import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    private var images = [PHAsset]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         populatePhotos()
     }
     
     private func populatePhotos() {
-        PHPhotoLibrary.requestAuthorization { status in
+        PHPhotoLibrary.requestAuthorization { [weak self ] status in
             if status == .authorized {
-                /*Access the hphotos from photo library*/
+                let assets = PHAsset.fetchAssets(with: .image, options: nil)
+                assets.enumerateObjects { (object, count, stop) in
+                    self?.images.append(object)
+                }
+                self?.images.reverse()
+//                self?.collectionView.reloadData()
             }
         }
     }
